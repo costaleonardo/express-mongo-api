533c491d280c54ca4403bbe9362efc01c45a090d
costaleonardo <costaleonardo97@gmail.com>
2018-08-28 16:23:49 -0400

    Add /middleware/passport.js.

---
+++ middleware/passport.js
@@ -0,0 +1,27 @@
+const { ExtractJWt, Strategy } = require('passport-jwt');
+const { User } = require('../models');
+const CONFIG = require('../config/config');
+const { to } = require('../services/util.service');
+
+module.exports = passport => {
+  let opts = {};
+
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+  opts.secretOrKey = CONFIG.jwt_encryption;
+
+  passport.use(new Strategy(opts, async (jwt_payload, done) => {
+    let err, user;
+
+    [err, user] = await to(User.findById(jwt_payload.user_id));
+
+    if (err) {
+      return done(err, false)
+    };
+
+    if (user) {
+        return done(null, user);
+    } else{
+        return done(null, false);
+    }
+  }));
+};
